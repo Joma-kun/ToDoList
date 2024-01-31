03d2e39f10aa07a1fad90b95d3cc189113439bb7
Joma-kun <jomamiwa@gmail.com>
2024-01-31 22:33:36 +0900

    add css

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -1,3 +1,4 @@
+import "./ToDoList.css"
 import {useState} from "react"
 
 function ToDoList() {
@@ -23,27 +24,27 @@ function ToDoList() {
     }
 
     return (
-        <div>
-            <h1>ToDoリスト</h1>
+        <div className="todoList">
+            <h1 className="title">ToDoリスト</h1>
 
             {/* 入力欄への変更(文字入力)をtodoに反映 */}
             <input value={todo} onChange={(e) => setTodo(e.target.value)} />
 
             {/* ボタン押下でhandlePushを実行 */}
-            <button onClick={handlePush}>登録</button>
+            <button onClick={handlePush} className="button">登録</button>
             <h2>タスク一覧</h2>
             <h3>完了</h3>
 
             {/* keyの変化(要素の追加・削除)を検知し，リストを更新 */}
-            <ul>
+            <ul className="ul">
                 {completedTodos.map((completedTodo, index) => (
-                    <li key={index}>{completedTodo}</li>
+                    <li key={index} className="li">{completedTodo}</li>
                 ))}
             </ul>
             <h3>未完</h3>
-            <ul>
+            <ul className="ul">
                 {todos.map((todo, index) => (
-                    <li key={index}>
+                    <li key={index} className="li">
                         {todo}
 
                         {/* ボタン押下でhandleCompleteを実行 */}
