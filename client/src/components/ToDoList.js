323a1db5c8ad3cfb5cd9ecd53161a09b54db17dc
Joma-kun <jomamiwa@gmail.com>
2024-02-08 15:37:25 +0900

    connect front to back

--- client/src/components/ToDoList.js
+++ client/src/components/ToDoList.js
@@ -1,26 +1,85 @@
+import axios from "axios"
 import "./ToDoList.css"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
 function ToDoList() {
     const [todos, setTodos] = useState([])
     const [todo, setTodo] = useState("")
     const [completedTodos, setCompletedTodos] = useState([])
 
+    // タスクを取得する
+    function handleGetTodos() {
+
+        // GETリクエストを送信
+        axios.get("http://localhost:8000/api/tasks").then((res) => {
+            let tempCompletedTodos = []
+            let tempTodos = []
+
+            // 配列の中身を確認し，完と未完に分類
+            for (let i=0; i<res.data.length; i++) {
+                if (res.data[i].finished) {
+                    tempCompletedTodos = tempCompletedTodos.concat([
+                        {
+                            id: res.data[i].id,
+                            name: res.data[i].name,
+                        },
+                    ])
+                } else {
+                    tempTodos = tempTodos.concat([
+                        {
+                            id: res.data[i].id,
+                            name: res.data[i].name,
+                        },
+                    ])
+                }
+            }
+            setCompletedTodos(tempCompletedTodos)
+            setTodos(tempTodos)
+        })
+    }
+
+    // ページを読み込んだ時に関数を実行
+    useEffect(() => {
+        handleGetTodos()
+    }, [])
+
     //タスクを追加する
     function handlePush() {
+
         ////todoが空文字列でない場合，新しくタスクを追加．その後入力欄を初期化
         if (todo !== "") {
-            setTodos(todos.concat([todo]))
+
+            // POSTリクエストを送信
+            axios.post("http://localhost:8000/api/tasks", {
+                name: todo,
+            }).then(() => {
+
+                // リストの中身を更新
+                handleGetTodos()
+            })
             setTodo("")
         }
     }
 
     //タスクを未完から完了にする
     function handleComplete(index) {
-        //index番のタスクを完了に追加
-        setCompletedTodos(completedTodos.concat(todos[index]))
-        //index番以外のタスクをスライスし，それらを未完に追加(更新)
-        setTodos(todos.slice(0, index).concat(todos.slice(index + 1)))
+        // PUTリクエストを送信(FalseをTrueにするだけなので送信データなし)
+        axios.put(`http://localhost:8000/api/tasks` + todos[index].id).then(() => {
+
+            // リストの中身を更新
+            handleGetTodos()
+        })
+    }
+
+    // タスクを削除する
+    function handleDelete(index) {
+
+        // DELETEリクエストを送信
+        axios.delete(`http://localhost:8000/api/tasks` + completedTodos[index].id).then(() => {
+            
+            // リストの中身を更新
+            handleGetTodos()
+        })
     }
 
     return (
@@ -38,14 +97,19 @@ function ToDoList() {
             {/* keyの変化(要素の追加・削除)を検知し，リストを更新 */}
             <ul className="ul">
                 {completedTodos.map((completedTodo, index) => (
-                    <li key={index} className="li">{completedTodo}</li>
+                    <li key={completedTodo.id} className="li">
+                        {completedTodo.name}
+
+                        {/* ボタン押下でhandleDeleteを実行 */}
+                        <button onClick={() => handleDelete(index)}>削除する</button>
+                        </li>
                 ))}
             </ul>
             <h3>未完</h3>
             <ul className="ul">
                 {todos.map((todo, index) => (
-                    <li key={index} className="li">
-                        {todo}
+                    <li key={todo.id} className="li">
+                        {todo.name}
 
                         {/* ボタン押下でhandleCompleteを実行 */}
                         <button onClick={() => handleComplete(index)}>完了する</button>
